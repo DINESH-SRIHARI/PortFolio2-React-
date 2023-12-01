2f87bd54fd3f5bb0190dbc423c534f7c76364501
srihari dinesh <dineshdinny>
2023-12-01 17:42:43 +0530

    version2

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,6 +6,8 @@ import arduino from "../images/arduino.png";
 import movie from "../images/movie bites.png";
 import portfo from "../images/Portfolio.jpg";
 import reg from "../images/Authentication.png";
+import audio from '../images/audio.png';
+import insta from '../images/webpage.png'
 import met from "../images/girl-attending-online-metting-through-group-call-vector-35719855.jpg";
 function Projects() {
   return (
@@ -27,6 +29,9 @@ function Projects() {
               Subsequently, the model is tested on a separate dataset to
               evaluate its performance.The model has successfully achieved an
               85% accuracy rate
+              <span style={{ color: "rgb(9, 124, 124)" }}>
+                  #Python,#FrontEnd,#flask,#SVM
+                </span>
             </p>
             <div className={style.a}>
               <a href="https://github.com/DINESH-SRIHARI/SpaceX-First-Stage-Success-Landing-Prediction">
@@ -64,6 +69,9 @@ function Projects() {
               analysis also unveiled the predominant gender influencing these
               transactions. This data-driven insight informs strategic decisions
               in e-commerce operations.
+              <span style={{ color: "rgb(9, 124, 124)" }}>
+                  #Python
+                </span>
             </p>
             <div className={style.a}>
               <a href="https://github.com/DINESH-SRIHARI/flipkart-sale-analysis">
@@ -102,10 +110,53 @@ function Projects() {
             <p>
               Designed and developed an intuitive online ordering platform for
               Shawarma streamline the online ordering process deployed in AWS
-              EC2 cloud services for Hosting.
+              EC2 cloud services for Hosting.<span style={{ color: "rgb(9, 124, 124)" }}>
+                  #FrontEnd,#Bootstrap,#php,#mysql,#AWS
+                </span>
             </p>
             <div className={style.a}>
               <a href="https://github.com/DINESH-SRIHARI/shawarma-online-ordering-page">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-link"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
+                  <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-github"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className={style.card}>
+          <img src={audio} className={style.img} />
+          <div className={style.mess}>
+            <p>
+              I developed and implemented an audio website enabling users to
+              convert images, PDF, or text to audio, providing downloadable
+              files.Using Python and Flask, I integrated a conversion library
+              for audio creation,  and
+              allowed file uploads.The user-friendly interface supports easy
+              navigation and customization of conversion options.
+              <span style={{ color: "rgb(9, 124, 124)" }}>
+                  #python,#flask,#FrontEnd
+                </span>
+            </p>
+            <div className={style.a}>
+              <a href="https://github.com/DINESH-SRIHARI/Echo-Audio-Converter">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
@@ -180,11 +231,13 @@ function Projects() {
             </div>
           </div>
           <div className={style.card1}>
-            <img src={reg} className={style.img1} />
+            <img src={insta} className={style.img1} />
             <div className={style.mess}>
               <p>
                 Authentication verification & upload image in real time using{" "}
-                <span style={{ color: "rgb(9, 124, 124)" }}>#ReactJS #firebase</span>{" "}
+                <span style={{ color: "rgb(9, 124, 124)" }}>
+                  #ReactJS #firebase
+                </span>{" "}
               </p>
               <div className={style.a}>
                 <a href="https://github.com/DINESH-SRIHARI/instagram-homepage">
@@ -210,9 +263,7 @@ function Projects() {
                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                   </svg>
                 </a>
-                <a href="https://postpixel.vercel.app/">
-                PostPixels.com
-                </a>
+                <a href="https://postpixel.vercel.app/">PostPixels.com</a>
               </div>
             </div>
           </div>
@@ -299,6 +350,9 @@ function Projects() {
             <p>
               Designed the heart beat and spo2 in a single sensor with cheap
               price and added messaging alter for the primary person
+              <span style={{ color: "rgb(9, 124, 124)" }}>
+                  #Arduino,#Iot
+                </span>
             </p>
             <div className={style.a}>
               <a href="https://drive.google.com/file/d/115BZP7dQyz46_avdqyNg3qKgumoBA3J3/view?usp=drivesdk">
